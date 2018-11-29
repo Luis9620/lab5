82d79a899238c6391fb4a746f42a6adee92b49a5
Luis Soto <43329793+Luis9620@users.noreply.github.com>
2018-11-28 22:42:35 -0700

    Update heapsort.py

--- heapsort.py
+++ heapsort.py
@@ -4,15 +4,21 @@ import unittest
 def heap_sort(number_list):
     H = Heap()
     for number in number_list:
+        H.print()
         H.insert(number)
+        H.print()
     for i in range(len(number_list)):
+        H.print()
         number_list[i] = H.extract_min()
+        H.print()
     return number_list
 
+
 if __name__ == '__main__':
-    number_list = [3,2,1,0]
+    number_list = [10,9,4,3,2,1,0]
     sorted_list = heap_sort(number_list)
     print(sorted_list)
+
 
 
 
